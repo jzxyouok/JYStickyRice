23155c286f248505d0885a0398c0f2e305b71480
mac mini <yu@macdemac-mini.local>
2016-03-12 21:15:39 +0800

    Initial Commit

---
+++ JYStickyRice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JYStickyRice
+//
+//  Created by mac mini on 16/3/12.
+//  Copyright © 2016年 JYStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JYStickyRice/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  JYStickyRice
+//
+//  Created by mac mini on 16/3/12.
+//  Copyright © 2016年 JYStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
